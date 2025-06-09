1e98eeb4bdd58fec4c59df71367e19d3ccf01122
Dante Nicola Capobianco <dantecapobianco@meta.com>
2025-06-08 21:38:46 -0700

    Add movie API key in env file, structure App component with header, main, and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,20 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-    
+      <header>
+
+      </header>
+      <main>
+
+      </main>
+      {/* modal pop-up goes outside main */}
+      <footer>
+
+      </footer>
     </div>
   )
 }
 
 export default App
+
+//todo: use button in index.css
